fec1517ce1a82f62d05ac38c2676cc83ee74c5f5
Mitch Davis <mitchell.alan.davis@gmail.com>
2015-03-07 15:14:48 -0600

    Refactor mithril module into pinger app

--- client/static/scripts/index.js
+++ client/static/scripts/index.js
@@ -1,25 +1,32 @@
-var Test = function() {
+var pinger = {};
+
+pinger.getTests = function() {
   return m.request({method: 'GET', url: '/api/tests/'});
 };
 
-var pinger = {
-  controller: function() {
-    var tests = Test();
-    return {
-      tests: tests,
+pinger.vm = (function() {
+    vm = {};
+    vm.init = function() {
+        vm.tests =  pinger.getTests();
     };
-  },
+    return vm;
+}());
 
-  view: function(ctrl) {
-    return m("div", [
-      m('h1', 'Pinger'),
-      m('ul', _.map(ctrl.tests(), function(test) {
-        return m('li', test.name);
-      })),
-    ]);
-  }
+pinger.controller = function() {
+    pinger.vm.init();
 };
 
+pinger.view = function() {
+    return m("div", [
+      m('h1', 'Pinger'),
+      m('ul', _.map(pinger.vm.tests(), function(test) {
+        return m('li', test.name);
+      }))
+    ]);
+};
+
+
+
 m.route(document.body, "/", {
-  "/": pinger,
+  "/": {controller: pinger.controller, view: pinger.view}
 });
