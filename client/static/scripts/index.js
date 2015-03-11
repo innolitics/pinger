835d24d180e7910fce7fb75fbd79822a37f31b5c
Mitch Davis <mitchell.alan.davis@gmail.com>
2015-03-11 10:23:24 -0500

    Add ping times and statuses to tests (for plotting)

--- client/static/scripts/index.js
+++ client/static/scripts/index.js
@@ -10,16 +10,21 @@ pinger.vm = (function() {
     vm = {};
     vm.init = function() {
         pinger.getTests()
-            .then(function(data){
-                vm.tests = data;
+            .then(function(tests){
+                vm.tests = _.map(tests, function(test){
+                    test.isUp = _.map(test.results, function(result){
+                        return result.status === 200;
+                    });
+                    test.pingTimes = _.map(test.results, function(result){
+                        return result.time;
+                    });
+                    return test;
+                });
                 vm.listen = (function () {
                     m.startComputation();
                     socket.on('newResult', function (data) {
                         try {
-                            testIndex = _.map(vm.tests, function(test){
-                                return test.id;
-                            }).indexOf(data.result.test_id);
-                            vm.tests[testIndex].results.push(data.result);
+                            vm.addResult(data.result);
                         } catch (e) {
                             alert('There is a problem:', e);
                         } finally {
@@ -29,6 +34,14 @@ pinger.vm = (function() {
                 }) ();
             });
 
+        vm.addResult = function(result){
+            testIndex = _.map(vm.tests, function(test){
+                return test.id;
+            }).indexOf(result.test_id);
+            vm.tests[testIndex].results.push(result);
+            vm.tests[testIndex].pingTimes.push(result.time);
+            vm.tests[testIndex].isUp.push(result.status === 200);
+        };
 
     };
     return vm;
@@ -39,12 +52,15 @@ pinger.controller = function() {
 };
 
 pinger.view = function() {
-    return m("div.container", [
+    var expanded_test = 3;
+    return m("#pinger-app", [
       m('h1', 'Pinger'),
       m('table.table', _.map(pinger.vm.tests, function(test) {
         return [m('table-head', test.url),
             m('td', _.map(test.results, function(result){
-                return m('tr', result.when);
+                if (expanded_test === test.id){
+                    return m('tr', result.when);
+                }
             }))
         ];
       }))
