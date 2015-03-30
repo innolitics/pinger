1e96df8f24018398734b4f0d8c4665f94687fedb
Mitch Davis <mitchell.alan.davis@gmail.com>
2015-03-30 16:20:15 -0500

    Switch to table view of last result

--- client/static/scripts/index.js
+++ client/static/scripts/index.js
@@ -33,7 +33,6 @@ pinger.vm = (function() {
 
     vm.addResult = function(result) {
         testIndex = _.pluck(vm.tests, 'id').indexOf(result.test_id);
-        vm.tests[testIndex].results.push(result);
         vm.tests[testIndex].pingTimes.push(result.time);
         vm.tests[testIndex].status.push(result.status);
     };
@@ -50,15 +49,25 @@ pinger.view = function() {
     var expanded_test = 3;
     return m("#pinger-app", [
       m('h1', 'Pinger'),
-      m('table.table', _.map(pinger.vm.tests, function(test) {
-        return [m('table-head', test.url),
-            m('td', _.map(test.results, function(result){
-                if (expanded_test === test.id){
-                    return m('tr', result.when);
-                }
-            }))
-        ];
-      }))
+      m('table.table', [
+        m('thead', 
+          m('tr', [
+            m('th', 'URL'),
+            m('th', 'Status'),
+            m('th', 'Time (ms)')
+          ])
+        ),
+        m('tbody', 
+          _.map(pinger.vm.tests, function(test) {
+          var latestPingTime = _.last(test.pingTimes);
+          var latestStatus = _.last(test.status);
+          return m('tr', [
+              m('td', test.url),
+              m('td', latestStatus),
+              m('td', latestPingTime),
+          ]);
+        }))
+      ])
     ]);
 };
 
